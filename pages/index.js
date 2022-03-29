d39cf5c01db67b358c2a42417820ae131eab0344
Ahmed Labeeb <labeeb.edu123@gmail.com>
2022-03-29 10:06:48 +0600

    issue fixed

--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,24 @@ import Layout from "./components/Layout";
 import { API_URL } from "../pages/config/index";
 
 export default function Home({events}) {
+  console.log(events)
   return (
     <Layout>
       <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map(evt => (
+        <h3 key={evt.id}>{evt.name}</h3>
+      ))}
     </Layout>
   );
 }
 
 export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/api/events]`);
+  const res = await fetch(`${API_URL}/api/events`);
   const events = await res.json();
   return {
-    props: {
-      events,
-    },
+    props: { events },
     revalidate: 1
   };
 }
