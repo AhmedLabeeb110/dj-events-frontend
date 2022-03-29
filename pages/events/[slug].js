2c9ccba8aa51accb391b1933a2d56f6b43395a5b
Ahmed Labeeb <labeeb.edu123@gmail.com>
2022-03-30 00:46:47 +0600

    front-end done

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,15 +1,61 @@
-import React from "react";
-// import { useRouter } from 'next/router'
+import { FaPencilAlt, FaTimes } from "react-icons/fa";
 import Layout from "../components/Layout";
+import styles from "../../styles/Event.module.css";
+import { API_URL } from "../config/index";
+import Link from "next/link";
+import Image from "next/image";
 
-export default function EventPage() {
-  // const router = useRouter()
-  // console.log(router)
+export default function EventPage({ evt }) {
+  const deleteEvent = (e) => {
+    console.log("delete");
+  };
 
   return (
     <Layout>
-      <div>EventPage</div>
+      <div className={styles.event}>
+        <div className={styles.controls}>
+          <Link href={`/events/edit/${evt.id}`}>
+            <a>
+              <FaPencilAlt /> Edit Event
+            </a>
+          </Link>
+          <a href="#" className={styles.delete} onClick={deleteEvent}>
+            <FaTimes /> Delete Event
+          </a>
+        </div>
+
+        <span>
+          {evt.name} at {evt.time}
+        </span>
+        <h1>{evt.name}</h1>
+        {evt.image && (
+          <div className={styles.image}>
+            <Image src={evt.image} width={960} height={600} />
+          </div>
+        )}
+        
+        <h3>Performers:</h3>
+        <p>{evt.performers}</p>
+        <h3>Description:</h3>
+        <p>{evt.description}</p>
+        <h3>Venue: {evt.venue}</h3>
+        <p>{evt.address}</p>
+
+        <Link href='/events'>
+          <a className={styles.back}>{'<'} Go Back</a>
+        </Link>
+      </div>
     </Layout>
-    // <p>{router.query.slug}</p>
   );
+}
+
+export async function getServerSideProps({ query: { slug } }) {
+  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const events = await res.json();
+  //console.log(slug)
+  return {
+    props: {
+      evt: events[0],
+    },
+  };
 }
